300fac446d1f966fd55932c36fa235eebcefb6eb
damonosius <dam195@yahoo.com>
2021-07-14 09:50:53 +0300

    am reparat headerul

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,66 +10,64 @@ const Header = () => {
   };
 
   return (
-    <>
-      <nav className="navbar">
-        <div className="nav-container">
-          <NavLink exact to="/" className="nav-logo">
-            <img src={companyLogo} alt="Arena Residence" />
-          </NavLink>
+    <nav className="navbar">
+      <div className="nav-container">
+        <NavLink exact to="/" className="nav-logo">
+          <img src={companyLogo} alt="Arena Residence" />
+        </NavLink>
 
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <NavLink
-                exact
-                className="nav-links"
-                activeClassName="active"
-                onClick={handleClick}
-                to="/home"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                className="nav-links"
-                activeClassName="active"
-                onClick={handleClick}
-                to="/map"
-              >
-                Apartamente
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                className="nav-links"
-                activeClassName="active"
-                onClick={handleClick}
-                to="/about"
-              >
-                About
-              </NavLink>
-            </li>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <NavLink
+              exact
+              className="nav-links"
+              activeClassName="active"
+              onClick={handleClick}
+              to="/home"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              className="nav-links"
+              activeClassName="active"
+              onClick={handleClick}
+              to="/map"
+            >
+              Apartamente
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              className="nav-links"
+              activeClassName="active"
+              onClick={handleClick}
+              to="/about"
+            >
+              About
+            </NavLink>
+          </li>
 
-            <li className="nav-item">
-              <NavLink
-                exact
-                className="nav-links"
-                activeClassName="active"
-                onClick={handleClick}
-                to="/contact"
-              >
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-          <div className="nav-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
-          </div>
+          <li className="nav-item">
+            <NavLink
+              exact
+              className="nav-links"
+              activeClassName="active"
+              onClick={handleClick}
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <div className="nav-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
