c44cfccbe9e2633e0f296bc0f205f4f805cd26db
damonosius <dam195@yahoo.com>
2021-08-28 10:38:41 +0300

    am schimbat si structura la data.js

--- src/components/apartamente/data.js
+++ src/components/apartamente/data.js
@@ -3,7 +3,6 @@ export const Data_c1 = [
     title: "Apartament 1",
     etaj: "parter",
     camere: "3",
-    suprafataUtila: "86.62 mp",
     scara: "C 1",
     liber: "true",
     descriere:
@@ -15,7 +14,6 @@ export const Data_c1 = [
     etaj: "parter",
     liber: "true",
     camere: "2",
-    suprafataUtila: "68 mp",
     scara: "C 1",
     descriere:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
@@ -26,7 +24,6 @@ export const Data_c1 = [
     etaj: "parter",
     liber: "true",
     camere: "2",
-    suprafataUtila: "69.77 mp",
     scara: "C 1",
     descriere:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
@@ -37,7 +34,6 @@ export const Data_c1 = [
     etaj: "et_1",
     liber: "true",
     camere: "3",
-    suprafataUtila: "69.37 mp",
     scara: "C 1",
     descriere:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
@@ -48,7 +44,446 @@ export const Data_c1 = [
     etaj: "et_1",
     liber: "true",
     camere: "2",
-    suprafataUtila: "66 mp",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 6",
+    etaj: "et_1",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 7",
+    etaj: "et_1",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 8",
+    etaj: "et_1",
+    liber: "true",
+    camere: "3",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 9",
+    etaj: "et_1",
+    liber: "true",
+    camere: "3",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 10",
+    etaj: "et_2",
+    liber: "true",
+    camere: "3",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 11",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 12",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 13",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 14",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 15",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 16",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 17",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 18",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 19",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 20",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 21",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 22",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 23",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 24",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 25",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 26",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 27",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 28",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 29",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 30",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 31",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 32",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 33",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 34",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 35",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 36",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 37",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 38",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 39",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 40",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 41",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 42",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 43",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 44",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 45",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 46",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 47",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 48",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
+    scara: "C 1",
+    descriere:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
+    id: 5,
+  },
+  {
+    title: "Apartament 49",
+    etaj: "et_2",
+    liber: "true",
+    camere: "2",
     scara: "C 1",
     descriere:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat iste distinctio eligendi adipisci atque, doloribus dolor hic harum voluptatem!",
