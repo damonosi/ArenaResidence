b33a62a969a1513987b4c01b6950fc30fa3fd314
damonosius <dam195@yahoo.com>
2021-09-08 16:01:55 +0300

    revamp bloc map

---
+++ src/components/map/AnimatedStores/C1/C1_Et1.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import { Spring, animated } from "@react-spring/konva";
+
+class C1Et1 extends React.Component {
+  state = { flag: false };
+  handleEnter = () => this.setState((state) => ({ flag: !state.flag }));
+  handleLeave = () => this.setState((state) => ({ flag: !state.flag }));
+  render() {
+    const { flag } = this.state;
+    return (
+      <Spring
+        native
+        from={{ opacity: 1 }}
+        to={{
+          opacity: flag ? 0.6 : 0,
+        }}
+      >
+        {(props) => (
+          <animated.Group
+            {...props}
+            onMouseLeave={this.handleLeave}
+            onMouseEnter={this.handleEnter}
+          >
+            <animated.Line
+              points={[640, 853, 640, 786, 97, 790, 98, 859]}
+              key={11}
+              // bezier={true}
+              // tension={0.1}
+              lineJoin={"round"}
+              fillAfterStrokeEnabled={true}
+              closed={true}
+              // stroke={"rgba(255, 186, 0, .7)"}
+              fill={"rgba(255, 186, 0, .7)"}
+            />
+
+            <animated.Label id="tooltip" x={300} y={820}>
+              <animated.Tag
+                fill={"#bbb"}
+                stroke={"#333"}
+                shadowColor={"black"}
+                shadowBlur={10}
+                shadowOffsetX={10}
+                shadowOffsetY={10}
+                shadowOpacity={0.5}
+                lineJoin={"round"}
+                pointerDirection={"left"}
+                pointerWidth={10}
+                pointerHeight={10}
+                cornerRadius={5}
+              />
+              <animated.Text
+                color="red"
+                text="Apasati pentru plan etaj 1"
+                fontSize={18}
+                padding={5}
+                fill="black"
+              />
+            </animated.Label>
+          </animated.Group>
+        )}
+      </Spring>
+    );
+  }
+}
+
+export default C1Et1;
---
+++ src/components/map/AnimatedStores/C2/C2_Et2.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import { Spring, animated } from "@react-spring/konva";
+
+class C2Et2 extends React.Component {
+  state = { flag: false };
+  handleEnter = () => this.setState((state) => ({ flag: !state.flag }));
+  handleLeave = () => this.setState((state) => ({ flag: !state.flag }));
+  render() {
+    const { flag } = this.state;
+    return (
+      <Spring
+        native
+        from={{ opacity: 1 }}
+        to={{
+          opacity: flag ? 0.6 : 0,
+        }}
+      >
+        {(props) => (
+          <animated.Group
+            {...props}
+            onMouseLeave={this.handleLeave}
+            onMouseEnter={this.handleEnter}
+          >
+            <animated.Line
+              points={[1167, 780, 1167, 722, 644, 722, 643, 782]}
+              key={11}
+              // bezier={true}
+              // tension={0.1}
+              lineJoin={"round"}
+              fillAfterStrokeEnabled={true}
+              closed={true}
+              // stroke={"rgba(255, 186, 0, .7)"}
+              fill={"rgba(255, 186, 0, .7)"}
+            />
+
+            <animated.Label id="tooltip" x={800} y={750}>
+              <animated.Tag
+                fill={"#bbb"}
+                stroke={"#333"}
+                shadowColor={"black"}
+                shadowBlur={10}
+                shadowOffsetX={10}
+                shadowOffsetY={10}
+                shadowOpacity={0.5}
+                lineJoin={"round"}
+                pointerDirection={"left"}
+                pointerWidth={10}
+                pointerHeight={10}
+                cornerRadius={5}
+              />
+              <animated.Text
+                color="red"
+                text="Apasati pentru plan etaj 2"
+                fontSize={18}
+                padding={5}
+                fill="black"
+              />
+            </animated.Label>
+          </animated.Group>
+        )}
+      </Spring>
+    );
+  }
+}
+
+export default C2Et2;
