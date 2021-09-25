f53316fe26933f9e56162dfe26d51c7da9eef91a
damonosius <dam195@yahoo.com>
2021-09-25 21:16:01 +0300

    finus

--- src/pages/Parteneri/ParteneriComponent.jsx
+++ src/pages/Parteneri/ParteneriComponent.jsx
@@ -5,14 +5,17 @@ import Schneider from "../../images/parteneri/Scheider.svg";
 import Porotherm from "../../images/parteneri/Porotherm2.svg";
 import Rehau from "../../images/parteneri/Rehau.svg";
 import "./Parteneri.scss";
+import { Parallax } from "react-scroll-parallax";
 
 const ParteneriComponent = () => {
   return (
     <div id="services" className="services-section backy">
-      <div className="sectionare ">
-        <div className="space">
-          <h1>Partenerii Nostrii</h1>
-        </div>
+      <div className="sectionare">
+        <Parallax x={[-5, 5]}>
+          <h1>
+            Partenerii <br /> Nostrii
+          </h1>
+        </Parallax>
       </div>
       <div className="content-wrapper">
         <img src={BarrierLogo} alt="1" />
