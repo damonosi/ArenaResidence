0f57a4913dc6163dbfc3acbb667f862077236afe
damonosius <dam195@yahoo.com>
2021-09-28 15:00:19 +0300

    new parteneri component + apartamente de la gabi

--- src/components/apartamente/Apartament.jsx
+++ src/components/apartamente/Apartament.jsx
@@ -16,9 +16,12 @@ const Apartament = () => {
           .map((filteredApartment) => (
             <div className="produs">
 
-              <h1 className="title">{filteredApartment.title}</h1>
+              <h1 className="title-apartament">{filteredApartment.title}</h1>
               <div className="fler">
                 <div className="info-apartament">
+                  <div className="plan-detaliat">
+                      <img src={filteredApartment.src} alt="plan detaliat" />
+                   </div>
                   <h4>Apartament cu {filteredApartment.camere} camere</h4>
                   <h8>{filteredApartment.descriere}</h8>
                   <p className="firstt">Etaj : {filteredApartment.etaj}</p>
@@ -31,9 +34,7 @@ const Apartament = () => {
                   </a>
                 </div>
 
-                   <div className="plan-detaliat">
-                      <img src={filteredApartment.src} alt="plan detaliat" />
-                   </div>
+                   
               </div>
 
                 
--- src/pages/Parteneri/ParteneriComponent.jsx
+++ src/pages/Parteneri/ParteneriComponent.jsx
@@ -1,30 +1,20 @@
 import React from "react";
 
-import BarrierLogo from "../../images/parteneri/barrier.svg";
-import Schneider from "../../images/parteneri/Scheider.svg";
-import Porotherm from "../../images/parteneri/Porotherm2.svg";
-import Rehau from "../../images/parteneri/Rehau.svg";
 import "./Parteneri.scss";
 import { Parallax } from "react-scroll-parallax";
 
+import ControlledCarousel from "./../../components/Carousel/Carousel";
 const ParteneriComponent = () => {
   return (
     <div id="services" className="services-section backy">
-      <div className="content-wrapper">
-        <div className="sectionare">
-          <Parallax x={[-5, 5]}>
-            <h1>
-              Partenerii <br /> Nostrii
-            </h1>
-          </Parallax>
-        </div>
-        <img src={BarrierLogo} alt="1" />
+      <Parallax x={[-5, 5]}>
+        <h1>
+          Partenerii <br /> Nostrii
+        </h1>
+      </Parallax>
 
-        <img src={Schneider} alt="2" />
-
-        <img src={Porotherm} alt="3" />
-
-        <img src={Rehau} alt="4" />
+      <div className="carousel-container">
+        <ControlledCarousel />
       </div>
     </div>
   );
