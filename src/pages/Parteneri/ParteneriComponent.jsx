05aaf8966da9d67f82fac0cea35a8cb1e009efba
damonosius <dam195@yahoo.com>
2021-10-13 17:12:06 +0300

    reparat bug galerie + nr contact +text nou

--- src/pages/Parteneri/ParteneriComponent.jsx
+++ src/pages/Parteneri/ParteneriComponent.jsx
@@ -1,26 +1,11 @@
 import React from "react";
 
 import "./Parteneri.scss";
-import { Parallax } from "react-scroll-parallax";
-import { Responsive } from "../../responsiveComponent/Responsive";
+
 import ControlledCarousel from "./../../components/Carousel/Carousel";
 const ParteneriComponent = () => {
   return (
     <div id="services" className="services-section backy">
-      <Responsive displayIn={["Laptop"]}>
-        <Parallax x={[-5, 5]}>
-          <h1>
-            Partenerii <br /> Nostrii
-          </h1>
-        </Parallax>
-      </Responsive>
-
-      <Responsive displayIn={["Mobile"]}>
-        <h1>
-          Partenerii <br /> Nostrii
-        </h1>
-      </Responsive>
-
       <div className="carousel-container">
         <ControlledCarousel />
       </div>
